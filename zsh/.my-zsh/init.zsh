85a736ce4caef3c5449824e7f97d7511d08e4e03
tiandic <2323748675@qq.com>
2026-07-11 13:44:56 +0800

    Commented out some plugins that are barely used.
    Changed the loading mode for plugins that only load after the first command.

--- zsh/.my-zsh/init.zsh
+++ zsh/.my-zsh/init.zsh
@@ -68,14 +68,14 @@ _deferred_init() {
   (( _deferred_init_done )) && return
   _deferred_init_done=1
 
-  zsh-defer source $MYZSH_PATH/plugins/zsh-you-should-use/you-should-use.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/ohmyzsh/plugins/git/git.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/ohmyzsh/plugins/web-search/web-search.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/ohmyzsh/plugins/jsontools/jsontools.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/ohmyzsh/plugins/docker/docker.plugin.zsh
-  zsh-defer source $MYZSH_PATH/plugins/ohmyzsh/plugins/docker-compose/docker-compose.plugin.zsh
+  source $MYZSH_PATH/plugins/zsh-you-should-use/you-should-use.plugin.zsh
+  source $MYZSH_PATH/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+  source $MYZSH_PATH/plugins/ohmyzsh/plugins/git/git.plugin.zsh
+  # source $MYZSH_PATH/plugins/ohmyzsh/plugins/web-search/web-search.plugin.zsh
+  # source $MYZSH_PATH/plugins/ohmyzsh/plugins/jsontools/jsontools.plugin.zsh
+  source $MYZSH_PATH/plugins/ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
+  # source $MYZSH_PATH/plugins/ohmyzsh/plugins/docker/docker.plugin.zsh
+  # source $MYZSH_PATH/plugins/ohmyzsh/plugins/docker-compose/docker-compose.plugin.zsh
 
   zsh-defer load_nvm
 }
