1b628356ecbabbe09af28d5a80432b2bbcdbf91a
tiandic <2323748675@qq.com>
2026-07-16 17:40:58 +0800

    Added 'gc' as an alias for 'git clone'.
    Improved the general alias setup so that an alias is only created if the command's executable exists.

--- zsh/.my-zsh/alias.zsh
+++ zsh/.my-zsh/alias.zsh
@@ -3,6 +3,15 @@ alias ll='ls -l'
 alias la='ls -A'
 alias l='ls -CF'
 
+try_set_alias(){
+  local target_cmd="$1"
+  local raw_cmd="$2"
+
+  local exec_name=$(echo "$raw_cmd" | awk '{print $1}')
+
+  command -v "$exec_name" &> /dev/null && alias "$target_cmd=$raw_cmd"
+}
+
 if cat /etc/os-release | grep -q -i debian ; then
   alias i="sudo apt install"
   alias u="sudo apt update"
@@ -19,12 +28,15 @@ elif cat /etc/os-release | grep -q -i arch ; then
 fi
 
 alias md="mkdir"
-alias py="python3"
-alias trm="trash-put"
-alias rcp="rsync -avhP --info=progress2 --no-inc-recursive"
-alias ipy="ipython3"
+try_set_alias py "python3"
+try_set_alias trm "trash-put"
+try_set_alias rcp "rsync -avhP --info=progress2 --no-inc-recursive"
+try_set_alias ipy "ipython3"
 
-alias ssh="kitty +kitten ssh"
-alias icat="kitty +kitten icat"
+# kitty
+try_set_alias ssh "kitty +kitten ssh"
+try_set_alias icat "kitty +kitten icat"
 
-alias gac="git add . && git commit -m"
+# git
+try_set_alias gac "git add . && git commit -m"
+try_set_alias gc "git clone"
