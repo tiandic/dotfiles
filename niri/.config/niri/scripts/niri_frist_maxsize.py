ebc2010db8d976e8ca7d5ca81911955e61f8f0e1
tiandic <2323748675@qq.com>
2026-07-22 10:23:57 +0800

    Added some comments for maintenance.

--- niri/.config/niri/scripts/niri_frist_maxsize.py
+++ niri/.config/niri/scripts/niri_frist_maxsize.py
@@ -67,10 +67,10 @@ def set_window_maxsize(con:Niri_con,id,workspace_id):
 def set_frist_window_half(con:Niri_con,workspace_id):
     if not str(workspace_id) in frist_window_ids:
         return
-    save_current_workspace_id(con)
+    save_current_workspace_id(con) # 改变窗口大小需要先聚焦到目标窗口,这会改变聚焦的工作区,出现进入niri时,聚焦到其他工作区,而非需要的工作区,所以需要先记录当前工作区
     frist_window_id=frist_window_ids[str(workspace_id)]
     del frist_window_ids[str(workspace_id)]
-    new_window_id=get_focused_window_id(con)
+    new_window_id=get_focused_window_id(con) # 打开新窗口时,会聚焦到新窗口,但调整旧窗口大小需要聚焦到旧窗口,在这里获取新窗口id,然后在调整旧窗口大小后恢复聚焦
 
     con.cmd({"Action": {"FocusWindow": {"id": frist_window_id}}})
     con.cmd({"Action": {"SetColumnWidth": {"change": {"SetProportion": 50.0}}}})
@@ -112,7 +112,7 @@ current_workspace_id=-1
 
 def main():
     c.send_cmd("EventStream")
-    update_all_windows()
+    update_all_windows()    # all_windows 用于获取关闭窗口的所在工作区 id
 
     while True:
         for k,v in c.read_line().items():
@@ -120,16 +120,18 @@ def main():
             if k in ["WindowOpenedOrChanged"]:
                 update_all_windows()
                 v=v["window"]
-                windows=get_workspace_windows(c2,v["workspace_id"])
+                windows=get_workspace_windows(c2,v["workspace_id"]) # 获取当前事件的工作区的所有窗口,用于判断应该最大化还是占一半
                 if len(windows)==1:
+                    # 如果只有工作区只有一个窗口则最大化
                     set_window_maxsize(c2,windows[0]["id"],v["workspace_id"])
                 elif len(windows)>1:
+                    # 如果有多个则各占显示器的一半
                     set_frist_window_half(c2,v["workspace_id"])
 
             elif k in ["WindowClosed"]:
                 workspace_id=get_workspace_id(v["id"])
                 update_all_windows()
-                windows=get_workspace_windows(c2,workspace_id)
+                windows=get_workspace_windows(c2,workspace_id) # 获取当前事件的所在工作区
                 if len(windows)==1:
                     set_window_maxsize(c2,windows[0]["id"],workspace_id)
 
